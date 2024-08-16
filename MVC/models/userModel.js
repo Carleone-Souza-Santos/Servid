6486bfcd37086b9891786fe323cbd9ef8633de8f
Carleone-Souza-Santos <carlleoneribeiro@hotmail.com>
2024-08-16 16:11:41 -0300

    aplic

---
+++ MVC/models/userModel.js
@@ -0,0 +1,17 @@
+// models/userModel.js
+let BancDados = [
+  { id: 1, name: 'Alice', age: 25 },
+  { id: 2, name: 'Bob', age: 30 },
+  { id: 3, name: 'Lara', age: 15 },
+];
+
+const getAllBancDados = () => {
+  return BancDados.sort((a , b) => a.age - b.age );
+}
+
+const addUserBancDados = (name, age) =>{
+  const id = BancDados.length + 1;
+  BancDados.push({ id, name, age });
+}
+
+module.exports = { getAllBancDados, addUserBancDados };
