2a42cc63429976c0998b4a05d353b891502ef0ce
Carleone-Souza-Santos <carlleoneribeiro@hotmail.com>
2024-08-16 16:09:38 -0300

    tst

---
+++ MVC/controllers/userController.js
@@ -0,0 +1,15 @@
+// controllers/userController.js
+const userModel = require('../models/userModel');
+
+function listUsers(req, res) {
+  const users = userModel.getAllBancDados();
+  res.render('users', { users });
+}
+
+function createUser(req, res) {
+  const { name, age } = req.body;
+  userModel.addUserBancDados(name, parseInt(age));
+  res.redirect('/users');
+}
+
+module.exports = { listUsers, createUser };
